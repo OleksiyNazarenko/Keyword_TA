164967fd838cade670e0ede90fb06c4d035c3f9b
OleksiyNazarenko <alexnazarenko23@gmail.com>
2023-05-25 18:49:50 +0200

    Initial commit

---
+++ src/main/java/utility/Constants.java
@@ -0,0 +1,8 @@
+package utility;
+public class Constants {
+    public static final String URL = "http://localhost:8090/login";
+    public static final String filePath = "C:\\Users\\olna0715\\Documents\\My Folder\\Univer\\Test_Data.xlsx";
+    public static final String excelData = "Test_Data.xlsx";
+    public static final String username = "admin";
+    public static final String password = "admin";
+}
